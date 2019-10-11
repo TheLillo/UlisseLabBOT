c9699aad71ee3752ba0c28f38e62c3b127e04f2e
gio <andrea.giovine@hotmail.it>
2019-10-11 10:05:28 +0200

    small fix script shell

--- generateVPNv2.sh
+++ generateVPNv2.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+if [ "$#" -eq 0 ] && [ -z "$1" ] && [ -z "$2" ]; then
+	echo " need user and group the same of UlisseLabBOT"
+	exit
+fi
+
+USER="$1"
+GROUP="$2"
+
+(nc -Ul /tmp/socketIPC.s &) ; sleep 1 && pkill netcat ; chown "$USER":"$GROUP" /tmp/socketIPC.s
+
 while true; do
-  USERNAME="$(nc -Ul -w 10 /tmp/socketIPC.s)"
+  USERNAME="$(nc -Ulk -w 10 /tmp/socketIPC.s)"
   ksh $(which generate_certificate.sh) "$USERNAME"
 done
