af132848b83814e0b62578c67e31f29d21d53bc9
gio <andrea.giovine@hotmail.it>
2019-11-23 10:29:33 +0100

    check if vpn already exist

--- manageVPN.py
+++ manageVPN.py
@@ -31,21 +31,24 @@ import time
 
 def check_or_gen_vpn(socket_address, vpn_dir, username):
     if Path(socket_address).exists():
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
-            client.connect(socket_address)
-            first_name_as_bytes = username.encode('utf-8')
-            name_sha256 = hashlib.sha256()
-            name_sha256.update(first_name_as_bytes)
-            first_name_hexdigest = name_sha256.hexdigest()
-            client.send(first_name_hexdigest.encode('utf-8'))
-            client.send("\n".encode('utf-8'))
-            # Controllo che sia avvenuta la creazione
-            time.sleep(2)
-            vpn_file = '{}{}.conf'.format(vpn_dir, str(first_name_hexdigest))
-            if Path(vpn_file).is_file():
-                return vpn_file
-            else:
-                print("Could not find VPN file :(", file=sys.stderr)
+        first_name_as_bytes = username.encode('utf-8')
+        name_sha256 = hashlib.sha256()
+        name_sha256.update(first_name_as_bytes)
+        first_name_hexdigest = name_sha256.hexdigest()
+        vpn_file = '{}{}.conf'.format(vpn_dir, str(first_name_hexdigest))
+        if Path(vpn_file).is_file():
+            return vpn_file
+        else:
+            with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+                client.connect(socket_address)
+                client.send(first_name_hexdigest.encode('utf-8'))
+                client.send("\n".encode('utf-8'))
+                # Controllo che sia avvenuta la creazione
+                time.sleep(2)
+                vpn_file = '{}{}.conf'.format(vpn_dir, str(first_name_hexdigest))
+                if Path(vpn_file).is_file():
+                    return vpn_file
+                else:
+                    print("Could not find VPN file :(", file=sys.stderr)
     else:
         print("Could not connect to the server :(", file=sys.stderr)
-
