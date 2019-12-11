4592ffbb876794e3cb5710f367803cbc43f833d2
gio <andrea.giovine@hotmail.it>
2019-12-11 16:45:59 +0100

    use kqueue to check if vpn is created

--- manageVPN.py
+++ manageVPN.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-# import os
+import os
 # import time
 import socket
 import sys
 import hashlib
-
+import platform
+import select
 
 # def check_or_gen_vpn(vpn_dir, temp_dir, first_name):
 #     # check if vpn is present in directory else generate
@@ -44,8 +45,26 @@ def check_or_gen_vpn(socket_address, vpn_dir, checker_new_vpn, username):
                 client.send(first_name_hexdigest.encode('utf-8'))
                 client.send("\n".encode('utf-8'))
                 # Controllo che sia avvenuta la creazione
-                #TODO: use Pynotify to check that vpn was create
+                if platform.system().casefold() == 'linux'.casefold():
+                    #TODO: use Pynotify to check that vpn was create
+                    raise NotImplementedError
+                elif platform.system().casefold() == 'openbsd'.casefold():
+                    kq = select.kqueue()
 
+                    fd = os.open(checker_new_vpn, os.O_DIRECTORY | os.O_RDONLY)
+
+                    ev = [select.kevent(fd,
+                                        filter=select.KQ_FILTER_VNODE,
+                                        flags=select.KQ_EV_ADD | select.KQ_EV_ENABLE | select.KQ_EV_CLEAR,
+                                        fflags=select.KQ_NOTE_WRITE
+                                        )]
+
+                    kq.control(ev, 1, None)
+                    os.fsync(fd)
+                    os.close(fd)
+                else:
+                    print("This Os is not supported :(", file=sys.stderr)
+                    
                 if Path(vpn_file).is_file():
                     return vpn_file
                 else:
