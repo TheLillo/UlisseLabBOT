e84a2cefad7fd429766fc2b6a61db0fde91c6881
plague <andrea.giovine@hotmail.it>
2020-03-21 01:51:57 +0100

    fix a path folder and unix permission

--- generateVPNv2.sh
+++ generateVPNv2.sh
@@ -7,23 +7,23 @@ SOCKET_DIR="$(dirname ${SOCKETFILE})"
 VPN_DIR="/etc/openvpn/clientconfigurations/"
 CHECKER_NEW_VPN="/tmp/finish"
 
-mkdir "$FOLDER_NEW_VPN"
-mkdir "${SOCKET_DIR}" || true
+mkdir "$CHECKER_NEW_VPN"
+mkdir -p "${SOCKET_DIR}"
 chown "${USER}:${GROUP}" "${SOCKET_DIR}"
 
-chmod 750 "${SOCKET_DIR}"
+chmod 777 "${SOCKET_DIR}"
 
 rm -f "${SOCKETFILE}"
 
 change_permission() {
   # Wait for the file creation and then change the permissions.
-  while ! test -e $1 ; do
+  while ! test -e "$1" ; do
     sleep .3
     echo "File not found sleeping"
   done
 
   echo "File found, changing permission"
-  chmod 660 $1
+  chmod 660 "$1"
 }
 
 change_permission "${SOCKETFILE}" &
@@ -31,6 +31,6 @@ change_permission "${SOCKETFILE}" &
 nc -klU "${SOCKETFILE}" | while read USERNAME; do
   if echo "${USERNAME}" | egrep -q '^[a-fA-F0-9]+$'; then
     ksh $(which generate_certificate.sh) "$USERNAME"
-    ln -s "$VPN_DIR""$USERNAME" "$FOLDER_NEW_VPN""$USERNAME"
+    ln -s "$VPN_DIR""$USERNAME" "$CHECKER_NEW_VPN""$USERNAME"
   fi
 done
