10dcd4e89983a0aaf92f3105e472347678aae21b
Davide Berardi <berardi.dav@gmail.com>
2020-07-18 12:19:53 +0200

    Handle multiple public key for user
    Now the bot can use multiple public key per user by enclosing
    them in an array.

--- managePublicKey.py
+++ managePublicKey.py
@@ -11,10 +11,10 @@ def add_public_key(public_keys_file, telegram_name, public_key):
             cur_yaml = yaml.safe_load(f)
             for player in cur_yaml['players']:
                 if player['telegram_name'] == telegram_name:
-                    player['ssh_key'] = public_key
+                    player['ssh_key'] += public_key
                     found = True
             if not found:
-                cur_yaml['players'].append({'name': telegram_name, 'telegram_name': telegram_name, 'ssh_key': public_key})
+                cur_yaml['players'].append({'name': telegram_name, 'telegram_name': telegram_name, 'ssh_key': (public_key)})
         except yaml.YAMLError as exc:
             print(exc, file=sys.stderr)
     if cur_yaml:
